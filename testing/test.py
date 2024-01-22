a8413d5b1ddb9653d42040b9bff0d051d546c01b
Ethan Frost <FRO18002750@btc.ac.uk>
2024-01-22 21:21:54 +0000

    Title added to header so the print would work in the python file, sleep added so chrome stayed open long enough to check everything

---
+++ testing/test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome()
+
+driver.get("http://localhost/A2(New)/index.php")
+
+print(driver.title)
+
+time.sleep(10)
