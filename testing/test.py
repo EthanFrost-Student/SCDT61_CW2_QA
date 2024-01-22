70e1ff0cc773fdd5af7441bd56f21d044785cb1a
Ethan Frost <FRO18002750@btc.ac.uk>
2024-01-22 23:02:51 +0000

    Comments are added and testing for the login page is done

--- testing/test.py
+++ testing/test.py
@@ -17,17 +17,28 @@ driver.get("http://localhost/A2(New)/index.php")
 #prints the title of the website GourmetGrocer
 print(driver.title)
 
-#Uisng the XPath method it finds the specified element then waits so I can visually identify it works then clicks the element
+#Using the XPath method it finds the specified element then waits so I can visually identify it works then clicks the element
 element = driver.find_element(By.XPATH, "/html/body/nav/a")
 time.sleep(8)
 element.click()
 
-#Using the XPATH it fins the equipment button in the header and click it
+
+#Using the XPATH it finds the equipment button in the header and clicks it
 element = driver.find_element(By.XPATH, "/html/body/nav/div/ul/li[2]/a")
 element.click()
-#sleep is used ahain to allow visualisation
+#sleep is used again to allow visualisation
 time.sleep(3)
 #Next it finds the GourmetGrocer button again and clicks it
+element = driver.find_element(By.XPATH, "/html/body/nav/a")
+element.click()
+
+
+#This finds the Login button on the index page and clicks it
+element = driver.find_element(By.XPATH, "/html/body/nav/div/ul/li[3]/a")
+element.click()
+#Sleep is used again for visualisation
+time.sleep(3)
+#Next the GourmetGrocer button is clicked on the login page
 element = driver.find_element(By.XPATH, "/html/body/nav/a")
 element.click()
 
