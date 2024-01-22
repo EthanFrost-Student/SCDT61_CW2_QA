b0d68f18cb0d33d2639d081026659f915e6017ea
Ethan Frost <FRO18002750@btc.ac.uk>
2024-01-22 22:47:24 +0000

    Written code to test the GourmetGrocer button in the header as well as the equipment button

--- testing/test.py
+++ testing/test.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
+from selenium.webdriver.common.by import By
+
 
 #Trying to keep Chrome open instead of instantly closing
 options = Options()
@@ -15,11 +17,19 @@ driver.get("http://localhost/A2(New)/index.php")
 #prints the title of the website GourmetGrocer
 print(driver.title)
 
+#Uisng the XPath method it finds the specified element then waits so I can visually identify it works then clicks the element
+element = driver.find_element(By.XPATH, "/html/body/nav/a")
+time.sleep(8)
+element.click()
 
-
-
-
-
+#Using the XPATH it fins the equipment button in the header and click it
+element = driver.find_element(By.XPATH, "/html/body/nav/div/ul/li[2]/a")
+element.click()
+#sleep is used ahain to allow visualisation
+time.sleep(3)
+#Next it finds the GourmetGrocer button again and clicks it
+element = driver.find_element(By.XPATH, "/html/body/nav/a")
+element.click()
 
 
 
