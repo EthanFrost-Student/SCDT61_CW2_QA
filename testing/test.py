b0e0c16b99af0d1498c6d60fc68e1b8d43fe5102
Ethan Frost <FRO18002750@btc.ac.uk>
2024-01-22 21:32:12 +0000

    Added comments

--- testing/test.py
+++ testing/test.py
@@ -3,13 +3,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
 
+#Trying to keep Chrome open instead of instantly closing
 options = Options()
 options.add_experimental_option("detach", True)
 
+#Driver being used is the Chrome one
 driver = webdriver.Chrome()
 
 driver.get("http://localhost/A2(New)/index.php")
 
+#prints the title of the website GourmetGrocer
 print(driver.title)
+
+
+
+
+
+
+
+
 
 time.sleep(20)
