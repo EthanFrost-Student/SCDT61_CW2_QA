71912bd409d10cfb81a4e44086b66d58640d3062
Ethan Frost <FRO18002750@btc.ac.uk>
2024-01-23 19:21:23 +0000

    Test the add equipment button's functionality

--- testing/test.py
+++ testing/test.py
@@ -20,24 +20,34 @@ print(driver.title)
 #This is here due to the fact that Selenium runs too quick and clicks the targetted area before the button loads in, giving an error, this allows the button to load first
 time.sleep(3)
 
+#This finds the Login button on the index page and clicks it
+element = driver.find_element(By.XPATH, "/html/body/nav/div/ul/li[3]/a")
+element.click()
+#Sleep is used again for visualisation
+time.sleep(3)
+#Finds the Not got an account? button on the login page
+element = driver.find_element(By.XPATH, "/html/body/form/section/div/div/div/div/div/a")
+element.click()
+##time.sleep(3)
+##element = driver.find_element(By.XPATH, "/html/body/nav/a")
+##element.click()
+
+
+
 
 #Using the XPATH it finds the equipment button in the header and clicks it
-element = driver.find_element(By.XPATH, "/html/body/nav/div/ul/li[2]/a")
-element.click()
-#sleep is used again to allow visualisation
-time.sleep(3)
-#This is finding another button on the equipment page and clicking it
-element = driver.find_element(By.XPATH, "/html/body/div/a")
-element.click()
-time.sleep(3)
-
-
-
-#This finds the Login button on the index page and clicks it
-##element = driver.find_element(By.XPATH, "/html/body/nav/div/ul/li[3]/a")
+##element = driver.find_element(By.XPATH, "/html/body/nav/div/ul/li[2]/a")
 ##element.click()
-#Sleep is used again for visualisation
+#sleep is used again to allow visualisation
 ##time.sleep(3)
+#This is finding another button on the equipment page and clicking it
+##element = driver.find_element(By.XPATH, "/html/body/div/a")
+##element.click()
+##time.sleep(3)
+
+
+
+
 
 
 
@@ -54,12 +64,7 @@ time.sleep(3)
 
 
 
-#Finds the Not got an account? button on the login page
-##element = driver.find_element(By.XPATH, "/html/body/form/section/div/div/div/div/div/a")
-##element.click()
-##time.sleep(3)
-##element = driver.find_element(By.XPATH, "/html/body/nav/a")
-##element.click()
+
 
 
 
