aba2fdbf7e6e1cc4a343535dc9efaae728ecc33a
Ethan Frost <FRO18002750@btc.ac.uk>
2024-01-23 20:01:45 +0000

    Clicks onto the input bar on the add equipment page and proceeds to input text into the bar

--- testing/test.py
+++ testing/test.py
@@ -12,7 +12,9 @@ options.add_experimental_option("detach", True)
 #Driver being used is the Chrome one
 driver = webdriver.Chrome()
 
-driver.get("http://localhost/A2(New)/index.php")
+##driver.get("http://localhost/A2(New)/index.php")
+##driver.get("http://localhost/A2(New)/register.php")
+driver.get("http://localhost/A2(New)/add_equipment.php")
 
 #prints the title of the website GourmetGrocer
 print(driver.title)
@@ -20,17 +22,13 @@ print(driver.title)
 #This is here due to the fact that Selenium runs too quick and clicks the targetted area before the button loads in, giving an error, this allows the button to load first
 time.sleep(3)
 
-#This finds the Login button on the index page and clicks it
-element = driver.find_element(By.XPATH, "/html/body/nav/div/ul/li[3]/a")
+#This clicks the input bar on the add equipment page
+element = driver.find_element(By.XPATH, "/html/body/div/form/div[1]/input")
 element.click()
-#Sleep is used again for visualisation
-time.sleep(3)
-#Finds the Not got an account? button on the login page
-element = driver.find_element(By.XPATH, "/html/body/form/section/div/div/div/div/div/a")
-element.click()
-time.sleep(4)
-element = driver.find_element(By.XPATH, "/html/body/form/section/div/div/div/div/div/a")
-element.click()
+#This is to ensure no error appears
+time.sleep(1)
+#This inputs text into the input bar that was previously clicked on
+element.send_keys("Testing the test!")
 
 
 
@@ -39,6 +37,12 @@ element.click()
 ##element.click()
 #sleep is used again to allow visualisation
 ##time.sleep(3)
+#Clicks the Add Equipment button on the equipment page
+##element = driver.find_element(By.XPATH, "/html/body/div/a")
+##element.click()
+
+
+
 #This clicks the edit button on the first row on the equipment page
 ##element = driver.find_element(By.XPATH, "/html/body/div/table/tbody[2]/tr[1]/td[4]/button")
 ##element.click()
@@ -46,6 +50,22 @@ element.click()
 ##time.sleep(2)
 #This finds the edit button on the second row of the equipment page and clicks it
 ##element = driver.find_element(By.XPATH, "/html/body/div/table/tbody[2]/tr[2]/td[4]/button")
+##element.click()
+
+
+
+
+#This finds the Login button on the index page and clicks it
+##element = driver.find_element(By.XPATH, "/html/body/nav/div/ul/li[3]/a")
+##element.click()
+#Sleep is used again for visualisation
+##time.sleep(3)
+#Finds the Not got an account? button on the login page
+##element = driver.find_element(By.XPATH, "/html/body/form/section/div/div/div/div/div/a")
+##element.click()
+##time.sleep(4)
+#This identical code finds the Already got an account? button on the register page, the reason it's the same is because it's in the same place as the previous button
+##element = driver.find_element(By.XPATH, "/html/body/form/section/div/div/div/div/div/a")
 ##element.click()
 
 
@@ -59,14 +79,6 @@ element.click()
 #Next it finds the GourmetGrocer button again and clicks it
 ##element = driver.find_element(By.XPATH, "/html/body/nav/a")
 ##element.click()
-
-
-
-
-#Next the GourmetGrocer button is clicked on the login page
-##element = driver.find_element(By.XPATH, "/html/body/nav/a")
-##element.click()
-
 
 
 
